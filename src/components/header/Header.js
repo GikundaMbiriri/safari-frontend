72b7ee0334275e42eeca96c6cbc904c3f2b91b98
GikundaMbiriri <petermbiriri8957@gmail.com>
2022-02-08 13:55:16 +0300

    dkdj

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import { Button } from 'antd';
 function Header() {
   return <div className='head'>
 
-     <div className='cname'>Safarixparts</div>
+     <div className='cname'>Book-A-Safari</div>
      <div className='auth'>
      <Button type="text" className='logout'>Logout</Button>
      <Button type="text" className='logout'>My Account</Button>
